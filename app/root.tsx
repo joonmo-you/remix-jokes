01ead0cb8cc7078ba574afbbc0e84e623a7cdcb4
Joonmo You <joonmo.you@gmail.com>
2023-03-26 21:22:34 +0900

    Styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,32 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+
+import globalStylesUrl from "~/styles/global.css";
+import globalMediumStylesUrl from "~/styles/global-medium.css";
+import globalLargeStylesUrl from "~/styles/global-large.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: globalStylesUrl,
+  },
+  {
+    rel: "stylesheet",
+    href: globalMediumStylesUrl,
+    media: "print, (min-width: 640px)",
+  },
+  {
+    rel: "stylesheet",
+    href: globalLargeStylesUrl,
+    media: "screen and (min-width: 1024px)",
+  },
+];
 
 export default function App() {
   return (
